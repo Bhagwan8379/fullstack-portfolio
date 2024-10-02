bce8840210b32e407d461b56670c185db4b17908
Bhagwan <bhagwangire05@gmail.com>
2024-10-02 13:58:14 +0530

    new setup

--- models/Carasoul.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const carouselSchema = new mongoose.Schema({
-    caption: { type: String },
-    hero: { type: String },
-
-}, { timestamps: true })
-
-module.exports = mongoose.model("carousel", carouselSchema)
